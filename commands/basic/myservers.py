2dd7e11d91d70f3c70bafcff7ec0c0da184dc30e
NisamVikki <viktorblazaric2@gmail.com>
2022-12-28 23:22:27 +0100

    Update

--- commands/basic/myservers.py
+++ commands/basic/myservers.py
@@ -1,41 +1,38 @@
 import interactions
 from pymongo import MongoClient
 from python_aternos import Client as AternosClient
+from python_aternos import Streams as AternosStreams
 import os
+import util.aternos_def as aternos_def
 
 
 class MyServers(interactions.Extension):
     def __init__(self, client):
         self.client: interactions.Client = client
 
-    @interactions.extension_command(name="my-servers", description="See a list of your servers", scope=1035996955869986859)
+    @interactions.extension_command(name="my-servers", description="See a list of your servers",
+                                    scope=1035996955869986859)
     async def myservers(self, ctx: interactions.CommandContext):
         await ctx.defer()
-        mongo = MongoClient(os.getenv("MONGO_URI"))
-        db = mongo["bot"]
-        collection = db["user_logins"]
-        userId = str(ctx.author.user.id)
-        login = collection.find_one({"id": userId})
+        aternos = aternos_def.get_login(str(ctx.author.user.id))
 
-        if login is not None:
-            aternos = AternosClient.from_credentials(login["username"], login["password"])
+        if aternos != "invalid_credentials" and aternos != "invalid_2fa" and aternos is not None:
             servers = aternos.list_servers()
 
-            # make a selection menu with the servers as options
             actionRow = interactions.ActionRow(components=[
                 interactions.SelectMenu(custom_id="myservers", placeholder="Select a server",
                                         options=[interactions.SelectOption(label=server.address, value=server.address)
                                                  for server in servers])])
 
             await ctx.send("Select a server", components=[actionRow])
+        else:
+            await ctx.send("You are not logged in!")
 
     @interactions.extension_component("myservers")
     async def myservers_select(self, ctx: interactions.CommandContext, serverAddress: str):
         await ctx.defer()
 
-        collection = MongoClient(os.getenv("MONGO_URI"))["bot"]["user_logins"]
-        login = collection.find_one({"id": str(ctx.author.user.id)})
-        aternos = AternosClient.from_credentials(login["username"], login["password"])
+        aternos = aternos_def.get_login(str(ctx.author.user.id))
         servers = aternos.list_servers()
 
         for server in servers:
@@ -46,36 +43,44 @@ class MyServers(interactions.Extension):
                     color=0x2B87D3,
                     fields=[
                         interactions.EmbedField(name="Status", value=f"**{server.status.upper()}**", inline=True),
-                        interactions.EmbedField(name="Players", value=f"{server.players_count}/{server.slots}", inline=True),
+                        interactions.EmbedField(name="Players", value=f"{server.players_count}/{server.slots}",
+                                                inline=True),
                         interactions.EmbedField(name="Memory", value=f"{server.ram}MB", inline=True),
-                        interactions.EmbedField(name="Edition", value=f"{server.version} {server.edition.name.capitalize()}, {server.software}", inline=True)
+                        interactions.EmbedField(name="Edition",
+                                                value=f"{server.version} {server.edition.name.capitalize()}, {server.software}",
+                                                inline=True)
                     ]
                 )
 
-                # make a button to start the server if it's not running already
+                aternos_def.select_server(str(ctx.author.user.id), server.address)
+
                 if server.status == "offline":
-                    action_row = interactions.ActionRow(components=[interactions.Button(label="Start", custom_id="startserver", style=interactions.ButtonStyle.SUCCESS)])
+                    action_row = interactions.ActionRow(components=[
+                        interactions.Button(label="Start", custom_id="startserver",
+                                            style=interactions.ButtonStyle.SUCCESS)])
                 else:
-                    action_row = interactions.ActionRow(components=[interactions.Button(label="Stop", custom_id="stopserver", style=interactions.ButtonStyle.DANGER)])
+                    action_row = interactions.ActionRow(components=[
+                        interactions.Button(label="Stop", custom_id="stopserver",
+                                            style=interactions.ButtonStyle.DANGER)])
 
                 await ctx.send(embeds=[embed], components=[action_row])
 
     @interactions.extension_component("startserver")
-    async def startserver(self, ctx: interactions.CommandContext, value: str):
+    async def startserver(self, ctx: interactions.CommandContext):
         await ctx.defer()
 
-        print(value)
+        server = aternos_def.get_server(str(ctx.author.user.id), aternos_def.get_selected_server(str(ctx.author.user.id)))
 
-        collection = MongoClient(os.getenv("MONGO_URI"))["bot"]["user_logins"]
-        login = collection.find_one({"id": str(ctx.author.user.id)})
-        aternos = AternosClient.from_credentials(login["username"], login["password"])
-        servers = aternos.list_servers()
+        if server is not None:
+            @server.wss().wssreceiver(AternosStreams.status)
+            async def on_message(msg):
+                print("received smth")
+                print(msg)
 
-        for server in servers:
-            if server.address == value:
-                server.start()
-
-                await ctx.send("Server started!")
+            await server.wss(True).connect()
+            server.start()
+        else:
+            await ctx.send("You are not logged in!")
 
     @interactions.extension_component("stopserver")
     async def stopserver(self, ctx: interactions.CommandContext):
@@ -91,5 +96,7 @@ class MyServers(interactions.Extension):
                 server.stop()
 
                 await ctx.send("Server stopped!")
+
+
 def setup(client):
     MyServers(client)
--- util/aternos_def.py
+++ util/aternos_def.py
@@ -8,35 +8,71 @@ def login(user_id, username, password, authcode=None):
     mongo = MongoClient(os.getenv("MONGO_URI"))
     db = mongo["bot"]
     collection = db["user_logins"]
-    user = collection.find_one({"id": user_id})
 
-    if user is not None:
-        try:
-            aternos = AternosClient.from_credentials(user["username"], user["password"])
-            print(aternos.saved_session)
-            return "ok"
-        except python_aternos.CredentialsError:
-            return "invalid_credentials"
-        except python_aternos.TokenError:
-            return "invalid_2fa"
-
-    else:
+    try:
+        AternosClient.from_credentials(username, password)
+        collection.insert_one({"id": user_id, "username": username, "password": password})
+        return "ok"
+    except python_aternos.CredentialsError:
+        return "invalid_credentials"
+    except python_aternos.TokenError:
+        return "invalid_2fa"
+    except Exception as e:
         return None
 
 
+def get_login(user_id):
+    mongo = MongoClient(os.getenv("MONGO_URI"))
+    db = mongo["bot"]
+    collection = db["user_logins"]
+    user = collection.find_one({"id": user_id})
+
+    try:
+        if user is not None:
+            aternos = AternosClient.from_credentials(user["username"], user["password"])
+            return aternos
+        else:
+            return None
+    except python_aternos.CredentialsError:
+        return "invalid_credentials"
+    except python_aternos.TokenError:
+        return "invalid_2fa"
+    except Exception as e:
+        return None
+
+
+def select_server(user_id, server_address):
+    mongo = MongoClient(os.getenv("MONGO_URI"))
+    db = mongo["bot"]
+    collection = db["user_logins"]
+    user = collection.find({"id": user_id})
+
+    if user is not None:
+        collection.update_one({"id": user_id}, {"$set": {"selected_server": server_address}})
+        return True
+    else:
+        return False
+
+
+def get_selected_server(user_id):
+    mongo = MongoClient(os.getenv("MONGO_URI"))
+    db = mongo["bot"]
+    collection = db["user_logins"]
+    user = collection.find_one({"id": user_id})
+
+    if user is not None:
+        return user["selected_server"]
+    else:
+        return False
+
+
 def get_server(user_id, server_address):
-    servers = login(user_id).list_servers()
+    servers = get_login(user_id).list_servers()
 
-    for server in servers:
-        if server.address == server_address:
-            return server
-
-    return None
-
-# function that can be used in another file  to get the server object
-#
-# import aternos_def
-#
-# server = aternos_def.get_server("user_id", "server_address")
-# print(server.address)
-
+    if servers is not None and servers != "invalid_credentials" and servers != "invalid_2fa":
+        for server in servers:
+            if server.address == server_address:
+                return server
+        return None
+    else:
+        return "failed_to_login"
